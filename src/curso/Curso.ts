856c9e512625b12cb832421a718c33007842df24
Hugo Coutinho <hugocoutinho96@outlook.com>
2025-07-10 19:11:52 -0300

    Curso:feat: adicionar e pegar primeiro e ultimo capitulo

--- src/curso/Curso.ts
+++ src/curso/Curso.ts
@@ -1,9 +1,10 @@
 import Entidade, { EntidadeProps } from "@/shared/Entidade";
-import { StringModule } from "@faker-js/faker/.";
 import Capitulo, { CapituloProps } from "./Capitulo";
 import Duracao from "@/shared/Duracao";
 import NomeSimples from "@/shared/NomeSimples";
 import Ordem from "@/shared/Ordem";
+import ErroValidacao from "@/error/ErroValidacao";
+import Erros from "@/constants/Erros";
 
 export interface CursoProps extends EntidadeProps {
     nome?: string,
@@ -33,6 +34,24 @@ export default class Curso extends Entidade<Curso, CursoProps> {
         this.capitulos = props.capitulos!.map(c => new Capitulo(c))
         this.duracao = new Duracao(props.duracao)
         this.quantidadeDeAulas = props.quantidadeDeAulas!
+
+        const {duracao, quantidadeDeAulas} = this.props
+        if(!duracao || duracao <= 0) ErroValidacao.lancar(Erros.CURSO_SEM_DURACAO, duracao)
+        if(quantidadeDeAulas! <= 0) ErroValidacao.lancar(Erros.CURSO_SEM_AULAS)
+    }
+
+    adicionarCapitulo(capitulo: Capitulo, posicao?: number): Curso {
+        const novosCapitulos = posicao !== undefined ? [...this.capitulos.slice(0, posicao), capitulo, ...this.capitulos.slice(posicao)] : [...this.capitulos, capitulo]
+        const capitulos = Curso.reatribuirOrdens(novosCapitulos).map(a => a.props)
+        return this.clone({capitulos})
+    }
+
+    get primeiroCapitulo() {
+        return this.capitulos[0]
+    }
+
+    get ultimoCapitulo() {
+        return this.capitulos[this.quantidadeDeAulas - 1]
     }
 
     private static calcularNumerosDoCurso(props: CursoProps) {
